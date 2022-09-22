7ea9b278bcc62cfbb9d0e5262d7abc1ba5e839f5
Phuong Anh <86072166+npa02@users.noreply.github.com>
2022-09-22 13:00:04 +0200

    Update Canvas.py
    Here is new

--- Canvas.py
+++ Canvas.py
@@ -1,11 +1,11 @@
 from triangle import *
 from block import *
-
-Canvasheight = 525
-Canvaswidth = 980
+CanvasHeight = 525
 
 BgColor = '#222'
-FG_COLOR = '#00000'
+FG_COLOR = '#000000'
+CanvasWidth = 1000
+CanvasHeight = 800
 from tkinter import *
 
 class Movefield:
@@ -20,35 +20,48 @@ class Movefield:
         self.relwidth = relwidth
         self.triangles = list()
         self.triangles_IDs = dict()
-        self.countdown = 0
+        self.countdown = 1
         self.FPS = FPS
         self.resethelp = 1  #helps to break update() loop
-
-        self.canvas = Canvas(self.root, bg=BgColor, width=Canvaswidth, height=Canvasheight)
+        self.canvas = Canvas(self.root, bg=BgColor, width=CanvasWidth, height=CanvasHeight)
         self.canvas.place(relx=self.relx, rely=self.rely,
                           relwidth=self.relwidth, relheight=self.relheight)
         self.block = Block(self)
+        self.Running = False
+
 
     def start(self): #helper for reset update()
         self.resethelp = 0
 
     def reset(self):
         self.resethelp = 1
-        # self.block.disappear()
-        #for i in self.triangles:
-        #    i.destruct()
+        self.canvas.delete('all')
+        self.block = Block(self)
         self.canvas.after(50, self.start())
         self.update()
+        # create 1 trangles
 
     def update(self):
             # create trangles
+        self.countdown -=1
+        if self.countdown == 0:
+            self.triangles.append(Triangle(self))
+        for i in self.triangles: 
+            if i.position < -i.width:
+                self.canvas.delete(i)
+                self.triangles.remove(i)
+            i.move()     
             # move triangles
             # delete triangles
             # collision check
-        self.countdown -= 1
         self.block.move()
         if self.resethelp == 0:
             self.canvas.after(1000 // self.FPS, self.update)  # remember to clear in reset
+
+    def stop(self):
+        self.resethelp = 1
+    
+
 
 
 
